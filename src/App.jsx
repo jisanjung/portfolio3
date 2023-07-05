dec9135d2f8771c2595d75ac017f42013df3858d
Jisan Jung <jjjung13@gmail.com>
2023-07-05 00:11:27 -0400

    chore: setup container

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import { Route, Routes } from 'react-router-dom';
 import About from './pages/About';
+import { useMediaQuery } from './hooks/useMediaQuery';
+import { SMALL_DESKTOP_WIDTH, LARGE_DESKTOP_WIDTH } from './constants';
 
 function App() {
 
+  const isSmallDesktop = useMediaQuery("(min-width: 1024px)");
+  const isLargeDesktop = useMediaQuery("(min-width: 1440px)");
+
+  const determineWidth = () => {
+    if (isLargeDesktop) return LARGE_DESKTOP_WIDTH;
+    if (isSmallDesktop) return SMALL_DESKTOP_WIDTH; 
+    return "auto";
+  }
+
   return (
-    <Routes>
-      <Route path='/' element={<About/>}/>
-    </Routes>
+    <main className='m-4 lg:flex lg:justify-center'>
+      <div style={{
+        width: determineWidth(),
+      }}>
+        <Routes>
+          <Route path='/' element={<About/>}/>
+        </Routes>
+      </div>
+    </main>
   )
 }
 
---
+++ src/constants.js
@@ -0,0 +1,3 @@
+// style variables
+export const SMALL_DESKTOP_WIDTH = "800px";
+export const LARGE_DESKTOP_WIDTH = "1000px";
