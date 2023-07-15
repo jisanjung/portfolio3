4cc18da0fd4e4aa5ef97ef11bf82bc6851901c4c
Jisan Jung <jjjung13@gmail.com>
2023-07-15 11:15:34 -0400

    style: add solid shadow to button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -13,14 +13,17 @@ const Button = ({ text, children }) => {
     }
 
   return (
-    <button className='bg-purple-600 hover:bg-purple-800 active:bg-purple-800 text-white py-4 px-9 lg:py-5 lg:px-12 xl:px-14 rounded-lg font-semibold' style={{
+    <div className='inline-block relative'>
+        <button className='bg-purple-600 hover:bg-purple-800 active:bg-purple-800 text-white py-4 px-9 lg:py-5 lg:px-12 xl:px-14 rounded-lg font-semibold relative z-10 active:top-2 active:right-2' style={{
         fontSize: determineTextSize(),
-    }}>
-        <div className='flex'>
-            <p className='mr-2'>{text}</p>
-            <span className='flex items-center'>{children}</span>
-        </div>
-    </button>
+        }}>
+            <div className='flex'>
+                <p className='mr-2'>{text}</p>
+                <span className='flex items-center'>{children}</span>
+            </div>
+        </button>
+        <div className='absolute top-2 -left-2 -bottom-2 right-2 bg-black rounded-lg'></div>
+    </div>
   )
 }
 
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,6 +3,7 @@ import Heading from '../components/Heading'
 import SubHeading from '../components/SubHeading'
 import Button from '../components/Button';
 import { IoMdArrowRoundForward } from "react-icons/io" 
+import { Link } from 'react-router-dom';
 
 const About = () => {
   return (
@@ -12,11 +13,13 @@ const About = () => {
         <SubHeading text="Hi there I'm Jisan."/>
         <SubHeading text="What I love doing most is solving business problems by implementing simple, friendly user interfaces."/>
       </div>
-      <Button text="See my work">
-        <IoMdArrowRoundForward className='text-xl lg:text-2xl xl:text-3xl' style={{
-          marginTop: "1px",
-        }}/>
-      </Button>
+      <Link to="/projects">
+        <Button text="See my work">
+          <IoMdArrowRoundForward className='text-xl lg:text-2xl xl:text-3xl' style={{
+            marginTop: "1px",
+          }}/>
+        </Button>
+      </Link>
     </section>
   )
 }
