fa71930aa430ccf1dcd36156c0c9dcbb7d92ab83
Jisan Jung <jjjung13@gmail.com>
2025-12-10 21:37:39 -0600

    fix paypal logo

--- src/constants.js
+++ src/constants.js
@@ -10,7 +10,7 @@ export const LARGE_DESKTOP_BREAKPOINT = "1536px";
 
 // misc variables
 export const MY_EMAIL = "jjjung13@gmail.com";
-export const PAYPAL_LOGO = "/src/assets/paypal_logo.png";
+export const PAYPAL_LOGO = "https://scontent-dfw5-2.xx.fbcdn.net/v/t39.30808-6/458326808_905214318303448_3160821425649911173_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=LsTql2beMg0Q7kNvwEkunVV&_nc_oc=AdmRjCajMj4AqJo04gHHGB9euEcAgaF0jBmMkvLZhn6cQn3USSttb2h8RR6mW0TbeYE&_nc_zt=23&_nc_ht=scontent-dfw5-2.xx&_nc_gid=E3wys0RwsisiMHlaB0P25A&oh=00_AflEKdqPKzkEuOCuHiMANT48vpPzglZ7U1sZy6gU71NpmQ&oe=69401158";
 export const ISLAND_NETWORKS_LOGO = "https://media.licdn.com/dms/image/C4D0BAQGZjAj6fdyhrg/company-logo_200_200/0/1519891185830?e=2147483647&v=beta&t=8cpJ7RgcKBA7I6UxgAT1sue9tv2CV2BXKs2NwDN-XBs";
 export const MC3_LOGO = "https://media.licdn.com/dms/image/D4E0BAQE9axB5b3fWGg/company-logo_200_200/0/1688562768590?e=2147483647&v=beta&t=G-lSOC9SODdXX9UCdvFi_BU9rPvt23qgxMpDseH_uMI";
 export const FORMSPREE_ID = "xyyoykke";
